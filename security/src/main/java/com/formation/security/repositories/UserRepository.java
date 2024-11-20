254dca64b71d31a66a6fcc210964f9fb7091b959
ClementHamonDev <clement.hamon35@gmail.com>
2024-11-20 16:42:09 +0100

    spring security filterChain config

--- security/src/main/java/com/formation/security/repositories/UserRepository.java
+++ security/src/main/java/com/formation/security/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.formation.security.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.formation.security.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String email);
+    public Optional<User> findByEmail(String email);
 }
